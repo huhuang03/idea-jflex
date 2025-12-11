d252889fd51b9b9f6ad3c330c552d3aa78fe71cf
huhuang03 <huhuang03@gmail.com>
2025-12-11 11:34:25 +0800

    feat: step fix compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,7 @@ dependencies {
         create("IC", "2025.1.4.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
-        // Add necessary plugin dependencies for compilation here, example:
-        // bundledPlugin("com.intellij.java")
+        bundledPlugin("com.intellij.java")
     }
 }
 
@@ -74,4 +73,9 @@ kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
+}
+
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
